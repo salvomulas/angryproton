a622b8b979deb336334283050fd4c4dc61e74b04
Salvatore Mulas <salvo@salvoblog.ch>
2015-11-17 00:17:11 +0100

    Environment prepared for Elixir, Form and HtmlFacade bootstrapped and required

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,21 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+var paths = {
+    'bootstrap': './node_modules/bootstrap-sass/assets/',
+    'fa': './node_modules/font-awesome/'
+}
+
+elixir(function (mix) {
+    mix.sass([
+            'app.scss',
+        ],
+        'public/css/style.css');
+    mix.scripts(paths.bootstrap + "javascripts/bootstrap.js", 'public/js/bootstrap.js');
+    mix.copy(paths.fa + "fonts/", "public/fonts");
+    mix.copy(paths.fa + "css/font-awesome.css", "public/css/font-awesome.css");
+    mix.copy(paths.bootstrap + "fonts/", 'public/fonts/');
+    mix.browserSync({
+        proxy: 'localhost/angryproton/public'
+    });
 });
