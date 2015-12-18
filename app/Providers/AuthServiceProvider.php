c0f3d232dcba815787c2de8bcc139123cddf9866
Salvatore Mulas <salvo@salvoblog.ch>
2015-12-18 14:16:04 +0100

    Bugfix: AuthServiceProvider throws exception if database is not yet migrated

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use App\Permission;
-use App\Role;
+use Illuminate\Database\QueryException;
 use Illuminate\Contracts\Auth\Access\Gate as GateContract;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -21,26 +21,28 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any application authentication / authorization services.
      *
-     * @param  \Illuminate\Contracts\Auth\Access\Gate  $gate
+     * @param  \Illuminate\Contracts\Auth\Access\Gate $gate
      * @return void
      */
     public function boot(GateContract $gate)
     {
         $this->registerPolicies($gate);
-        /**
-        foreach ($this->getPermissions() as $permission) {
-            $gate->define($permission->name, function ($user) use ($permission) {
-                $user->hasRole($permission->roles);
-            });
-        }
-         */
 
+        // Wrapped within try/catch to avoid migration problems in testing
+        try {
+            foreach ($this->getPermissions() as $permission) {
+                $gate->define($permission->name, function ($user) use ($permission) {
+                    $user->hasRole($permission->roles);
+                });
+            }
+        } catch (QueryException $e) {
+            return false;
+        }
     }
 
-    /**
     protected function getPermissions()
     {
         return Permission::with('roles')->get();
     }
-    */
+
 }
